7e8353a40046ae767fccec743bdd059906cf3524
youymi <youymi@163.com>
2014-08-08 16:52:49 +0800

    单点登录集成时判断用户是否已经存在本地库，没有的话插入

--- routers/epicaccount/epicAccount.go
+++ routers/epicaccount/epicAccount.go
@@ -1,8 +1,11 @@
 package epicaccount
 
 import (
+	"fmt"
 	"github.com/EPICPaaS/wetalk/modules/auth"
+	"github.com/EPICPaaS/wetalk/modules/models"
 	"github.com/astaxie/beego"
+	"strconv"
 )
 
 type EpicAccount struct {
@@ -12,9 +15,43 @@ type EpicAccount struct {
 func (this *EpicAccount) Login() {
 	token := this.GetString("token")
 	result := auth.VerifyToken(token)
+
+	fmt.Println(result.Username)
+	fmt.Println(result.Userid)
+	fmt.Println(result.Email)
+
 	if !result.Succeed {
 		this.Data["json"] = "failed"
 	} else {
+		//query weather this user exist in wetalk db
+		userNew := models.User{}
+		userid, err := strconv.Atoi(result.Userid)
+		if err != nil {
+			fmt.Println("用户Id转换出错了:" + err.Error())
+		}
+
+		//fmt.Println(result.Userid)
+
+		userNew.Id = userid
+		err = userNew.Read("Id")
+		if err == nil {
+			fmt.Println("获取用户信息失败 - nil")
+		} else {
+			fmt.Println("找不到用户 userId - " + result.Userid)
+			userNew.UserName = result.Username
+			userNew.Email = result.Email
+			fmt.Println(userNew.Id)
+
+			err = userNew.Insert()
+
+			//if err == nil {
+
+			//} else {
+			//	fmt.Println("插入用户信息出错 -" + err.Error())
+			//}
+
+		}
+
 		this.Ctx.SetCookie("epic_user_token", token, "/")
 		this.Ctx.ResponseWriter.Header().Add("P3P", "CP='IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT'")
 		this.Data["json"] = "succeed"
